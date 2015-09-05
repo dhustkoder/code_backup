2615a0f33e5b6bf4304d90b26e52412006f8d2ac
dhustkoder <rafaelmoura.dev@gmail.com>
2015-09-05 00:32:21 +0000

    RAII intro

---
+++ Cpp/ACPP_List/RAII/raii_1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <mutex>
+
+
+
+/*  Resource Aquisition Is Initialization ( RAII )
+ *
+ * 	Use Objects to manage resources :
+ * 		memory, hardware device, network connection , etc.
+ *
+ *
+ *
+ */
+
+
+
+
+
+
+
+
+int main()
+{
+	std::mutex mu;
+	mu.lock();
+
+
+
+
+}
+
+
+
+
+
+
+
+
