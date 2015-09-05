361836c32cb26605629c577b6412bd04f11a9ec7
dhustkoder <rafaelmoura.dev@gmail.com>
2015-09-05 00:59:09 +0000

    controlling threads

--- Cpp/ACPP_List/RAII/raii_1.cpp
+++ Cpp/ACPP_List/RAII/raii_1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdio>
 #include <chrono>
 #include <thread>
 #include <mutex>
@@ -10,18 +11,36 @@
  * 	Use Objects to manage resources :
  * 		memory, hardware device, network connection , etc.
  *
- *
+ * 	Example number 1: controlling threads.
  *
  */
+std::mutex thread_controll;
 
 
-
-
-void some_thread_shared_function()
+void print_message()
 {
 
+	std::cout << "I'm thread " << std::this_thread::get_id() << std::endl;
+}
 
 
+
+void some_thread_shared_function(const int n_times)
+{
+
+	thread_controll.lock();
+				
+	
+	
+	
+	for(int i= 0; i < n_times; ++i) // Let's say that , "print_message" may throw an exception
+		print_message();
+		
+	
+	
+	
+	
+	thread_controll.unlock();	
 
 
 
@@ -31,10 +50,15 @@ void some_thread_shared_function()
 
 int main()
 {
-	std::mutex mu;
-	mu.lock();
+	std::thread thread_1(some_thread_shared_function,4);
+	std::thread thread_2(some_thread_shared_function,2);
 
-	std::this_thread::sleep_for(std::chrono::seconds(5));
+	thread_2.join();
+	thread_1.join();
+
+
+
+//	std::this_thread::sleep_for(std::chrono::seconds(5));
 
 
 }
