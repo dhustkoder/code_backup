5d4a7e6463896e6a3725ab85953a8ef6ddbac800
dhustkoder <rafaelmoura.dev@gmail.com>
2015-09-19 03:19:14 +0000

    Type Conversion

--- Cpp/ACPP_List/Type Conversion/Implicit User Defined Type Conversion/implicit_user_defined.cpp
+++ Cpp/ACPP_List/Type Conversion/Implicit User Defined Type Conversion/implicit_user_defined.cpp
@@ -9,7 +9,8 @@ public:
 		skills_ = "BigBoss Skills: A++, A++, A++, A++, A++";
 	
 	}		
-	operator const char*() { return skills_.c_str(); } 
+	const char* getSkills() { return skills_.c_str(); }
+	operator const char*() { return skills_.c_str(); } // no need for getSkills anymore. 
 
 private:
 	std::string skills_;
@@ -26,20 +27,27 @@ private:
 int main()
 {
 
-	// User Defined Type Conversion.
-
-
-
-
-
-
+	/* User Defined Type Conversion.
+	 * These are defined inside a class or struct
+	 *
+	 * just like that, imagine I want to get the main std::string, that represent BigBoss object
+	 * 
+	 * well , sometimes is better not to use it, some other times might be good to use, for math operations for example.
+	 *
+	 *
+	 *
+	 *
+	 *
+	 *
+	 *
+	 *
+	 */
 
 
 
 	BigBoss boss; 
-	std::cout << boss << std::endl;
-
-
+	std::cout << boss.getSkills() << std::endl;
+	std::cout << boss << std::endl; // less code
 
 
 
