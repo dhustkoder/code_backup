d266eb95561e57f2f9a0b5edd49b27a9351c5fbd
dhustkoder <rafaelmoura.dev@gmail.com>
2015-09-19 03:51:44 +0000

    user defined

--- Cpp/ACPP_List/Type Conversion/Implicit User Defined Type Conversion/implicit_user_defined.cpp
+++ Cpp/ACPP_List/Type Conversion/Implicit User Defined Type Conversion/implicit_user_defined.cpp
@@ -4,22 +4,25 @@
 class BigBoss
 {
 public:
-	BigBoss()
+	BigBoss(std::string name = "Jhon")
 	{
+		name_ = name;
 		skills_ = "BigBoss Skills: A++, A++, A++, A++, A++";
 	
-	}		
-	const char* getSkills() { return skills_.c_str(); }
-	operator const char*() { return skills_.c_str(); } // no need for getSkills anymore. 
+	}
+	// If you only want to define a contructor, and no implicit type conversion,
+	// always put "explicit" before the constructor to avoid inadvertent type conversion.		
+	const char* getProfile() { return std::string( name_ + "\n" + skills_ ).c_str(); }
+
+	operator const char*() { return std::string( name_ + "\n" + skills_ ).c_str(); } // no need for getProfile anymore. 
 
 private:
+	std::string name_;
 	std::string skills_;
 
 
 
 };
-
-
 
 
 
@@ -45,8 +48,15 @@ int main()
 
 
 
-	BigBoss boss; 
-	std::cout << boss.getSkills() << std::endl;
+	BigBoss boss(bossu); 
+
+
+	std::cout << "\nusing function...\n" << std::endl;
+
+	std::cout << boss.getProfile() << std::endl;
+
+	std::cout << "\nusing implicit type conversion...\n" << std::endl;
+
 	std::cout << boss << std::endl; // less code
 
 
