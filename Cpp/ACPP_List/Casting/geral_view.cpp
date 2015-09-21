1b3f17daf24f66630280b3d368d87a61793a10bd
dhustkoder <rafaelmoura.dev@gmail.com>
2015-09-21 18:21:35 +0000

    dynamic intro

--- Cpp/ACPP_List/Casting/geral_view.cpp
+++ Cpp/ACPP_List/Casting/geral_view.cpp
@@ -5,6 +5,7 @@ class Dog
 friend std::ostream& operator<<(std::ostream &os, Dog &dog)
 {
 	std::cout << "This is a dog named: " << dog.name_ << std::endl; 
+	return os;
 } 
 public:
 	Dog(std::string name) :
@@ -44,7 +45,8 @@ int main()
 
 
 
-	// 1. Static cast
+	// 1. Static_Cast
+	// the static_cast can work on all types, as long as type conversion is defined for those types...
 
 	int i = 10;
 
@@ -63,20 +65,39 @@ int main()
 
 	std::cout << dog1 << std::endl;
 	std::cout << *dog2 << std::endl; // this will enter the same friend function as the dog1, cause is casted to a Dog* pointer...
-	Test *ptr = new Test;
-	Test t = static_cast<Test>(*(ptr)); // cool, the Test is destroyed in end of scope...
+	
+
+	{
+		Test *ptr = new Test;
+		Test t = static_cast<Test>(*(ptr)); // cool, the Test is destroyed in end of scope...
 						// but dont be stupid, the Test allocated with new is still there...
 
 	
+		delete ptr; // now it really is destroyed
+	}
+
+
+
+	// 2. Dynamic_Cast
+	// the dynamic_cast convert a pointer/reference to a related type (down cast)
+
+	Dog *d3 = new YellowDog("STEAM"); // this is a dynamic_cast, even if im not using the operator.
+
+	YellowDog *d4 = dynamic_cast<YellowDog*>(d3); // doing downcast, casting from base class to derived...
+
+	// dynamic_cast do run-time check, if the types are compatible , d4 == d3, if not d4 == nullptr.
+
+	delete d4;
+	delete d3;
+
 	
+
 	
 
-	delete ptr; // now it really is destroyed
 
 
 
-
-
+	return 0;
 
 }
 
