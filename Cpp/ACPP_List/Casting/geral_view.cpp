7a2f20762de5d8b24bd613075b3da415855dfa6a
dhustkoder <rafaelmoura.dev@gmail.com>
2015-09-21 16:16:50 +0000

    casting

---
+++ Cpp/ACPP_List/Casting/geral_view.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <typeinfo>
+class Dog
+{
+friend std::ostream& operator<<(std::ostream &os, Dog &dog)
+{
+	std::cout << "This is a dog named: " << dog.name_ << std::endl; 
+} 
+public:
+	Dog(std::string name) :
+		name_ ( name ) {}
+private:
+	std::string name_;
+
+};
+
+
+class YellowDog : public Dog
+{
+public:
+	YellowDog(std::string name) :
+		Dog(name) {}
+
+
+};
+
+
+
+
+
+int main()
+{
+	// Type Conversion:
+	// 
+	// 1. Implicit Type Conversion
+	// 2. Explicit Type Conversion - Casting
+
+
+
+	// 1. Static cast
+
+	int i = 10;
+
+	float f = static_cast<float>(i); // convert one type to another
+
+	Dog dog1 = static_cast<Dog> (std::string("Diamond Dog")); // this type conversion needs to be defined in the constructor of dog to initialize it in this way.
+															// constructor without keyword "explicit"
+
+	Dog* dog2 = static_cast<Dog*>(new YellowDog("Patric")); // convert pointer/reference from one type to a related type (up/down casting)
+
+
+
+	std::cout << dog1 << std::endl;
+	std::cout << *dog2 << std::endl; // this will enter the same friend function as the dog1, cause is casted to a Dog* pointer...
+
+
+	
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
