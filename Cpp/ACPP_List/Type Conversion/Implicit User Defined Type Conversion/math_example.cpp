252ab15da5eb38a3b05e5ea63093ccf7bc8c922c
dhustkoder <rafaelmoura.dev@gmail.com>
2015-09-21 09:16:26 +0000

    implicit conversions math

--- Cpp/ACPP_List/Type Conversion/Implicit User Defined Type Conversion/math_example.cpp
+++ Cpp/ACPP_List/Type Conversion/Implicit User Defined Type Conversion/math_example.cpp
@@ -21,15 +21,21 @@ class Rational
 
 
 public:
-	Rational(int numerator, int dennominator) : 
+	Rational(int numerator = 0, int dennominator = 1) : 
 		numerator_ ( numerator ), dennominator_ ( dennominator ){}
 	
-	const Rational operator*(Rational &rhs)
+	const Rational operator*(Rational rhs)
 	{
 		return Rational( numerator_ * rhs.numerator_ , dennominator_ * rhs.dennominator_ );
 	}
-
-
+	const Rational operator*(int multiplier)
+	{
+		return Rational(numerator_ * multiplier, dennominator_ * multiplier);
+	}	
+	operator int()
+	{
+		return ( numerator_ / dennominator_ );
+	}
 private:
 	int numerator_, dennominator_;
 
@@ -40,13 +46,25 @@ private:
 
 int main()
 {
-	Rational rat(5,10);
+	Rational rat  = 20; // 20 is implicit converted into Rational
+	
+	std::cout << rat; // 20, 1
 
-	rat = rat * rat;
+	
+	Rational rat2 = rat * 2; // operator*(int) is called
+	
+	
+	std::cout << rat2; // 40, 2 
+	
+	
+	Rational rat3 = rat * rat2; // operator*(const Rational&) is called
 
-	std::cout << rat;	
-	std::cout << rat;
+	
+	std::cout << rat3; // 800, 2
 
+	Rational rat4 = 2 * rat3; // operator int() is called
+
+	std::cout << rat4; // 800, 1
 
 }
 
