64d141bd1fe5e81130cca841deca9456ad6eb684
dhustkoder <rafaelmoura.dev@gmail.com>
2015-09-22 05:27:37 +0000

    geral cast intro done

--- Cpp/ACPP_List/Casting/geral_view.cpp
+++ Cpp/ACPP_List/Casting/geral_view.cpp
@@ -2,15 +2,25 @@
 #include <typeinfo>
 class Dog
 {
-friend std::ostream& operator<<(std::ostream &os, Dog &dog)
-{
-	std::cout << "This is a dog named: " << dog.name_ << std::endl; 
-	return os;
-} 
+	friend std::ostream& operator<<(std::ostream &os, Dog &dog)
+	{
+		std::cout << "This is a dog named: " << dog.name_ << std::endl; 
+		return os;
+	} 
 public:
 	Dog(std::string name) :
 		name_ ( name ) {}
-private:
+
+	virtual void iAm() { std::cout << "I'm a Dog named: " << this->name_ << std::endl; }
+	
+
+	~Dog()
+	{
+		std::cout << "Dog " << this->name_ << ", destroyed" << std::endl;
+	}
+
+
+protected:
 	std::string name_;
 
 };
@@ -21,17 +31,26 @@ class YellowDog : public Dog
 public:
 	YellowDog(std::string name) :
 		Dog(name) {}
-	void imYellow(){ std::cout << "Im Yellow" << std::endl; };
 
+	void iAm()
+	{ 
+		std::cout << "Im Yellow" << std::endl; 
+	}
+
+	~YellowDog()
+	{
+		std::cout << "YellowDog " << this->name_ << ", Destroyed" << std::endl;
+	}
 };
 
 class Test
 {
 public:
-~Test()
-{
-	std::cout << "DESTROYED TEST" << std::endl;
-}
+	
+	~Test()
+	{
+		std::cout << "DESTROYED TEST" << std::endl;
+	}
 };
 
 
@@ -46,6 +65,8 @@ int main()
 
 
 	// 1. Static_Cast
+
+
 	// the static_cast can work on all types, as long as type conversion is defined for those types...
 
 	int i = 10;
@@ -58,19 +79,20 @@ int main()
 	Dog* dog2 = static_cast<Dog*>(new YellowDog("Patric")); // convert pointer/reference from one type to a related type (up/down casting)
 
 	//dog2->imYellow(); // doesn't work now
-	static_cast<YellowDog*>(dog2)->imYellow(); // now it works...
+	static_cast<YellowDog*>(dog2)->iAm(); // now it works...
 	// also works for a type that is not intended to be YellowDog... 
-	static_cast<YellowDog*>( & dog1 )->imYellow(); // so becareful, you might fuck your code up. if starts throwing pointers and casts between related types
+	static_cast<YellowDog*>( & dog1 )->iAm(); // so becareful, you might fuck your code up. if starts throwing pointers and casts between related types
 
 
 	std::cout << dog1 << std::endl;
 	std::cout << *dog2 << std::endl; // this will enter the same friend function as the dog1, cause is casted to a Dog* pointer...
 	
+	delete dog2;
 
 	{
 		Test *ptr = new Test;
 		Test t = static_cast<Test>(*(ptr)); // cool, the Test is destroyed in end of scope...
-						// but dont be stupid, the Test allocated with new is still there...
+											// but dont be stupid, the Test allocated with new is still there...
 
 	
 		delete ptr; // now it really is destroyed
@@ -79,24 +101,34 @@ int main()
 
 
 	// 2. Dynamic_Cast
-	// the dynamic_cast convert a pointer/reference to a related type (down cast)
 
-	Dog *d3 = new YellowDog("STEAM"); // this is a dynamic_cast, even if im not using the operator.
 
-	YellowDog *d4 = dynamic_cast<YellowDog*>(d3); // doing downcast, casting from base class to derived...
+	// the dynamic_cast only work on pointer/reference to a related type (down cast)
 
-	// dynamic_cast do run-time check, if the types are compatible , d4 == d3, if not d4 == nullptr.
+	Dog *baseDog = new Dog("BASE DOG");
+	Dog *derivedDog = new YellowDog("DERIVED DOG"); // this is a dynamic_cast, even if im not using the operator.
 
-	delete d4;
-	delete d3;
-
+	YellowDog *yellowDog = dynamic_cast<YellowDog*>(derivedDog); // doing downcast, casting from base class to derived...
+												  // this works, cause derivedDog points to a YellowDog
 	
 
+	YellowDog *yellowDog_2 = dynamic_cast<YellowDog*>(baseDog); // trying to make a Dog* into a  YellowDog* ( derived type), this won't work, 
+																//and *yellowDog_2 will point null
+
+	// dynamic_cast do run-time check, if the types are compatible , yellowDog_2 == baseDog, if not yellowDog_2 == nullptr.
+
+
+	if(yellowDog_2 == nullptr)
+		std::cout << "yellowDog_2 is nullptr, dynamic_cast failed" << std::endl;
+
+
+
+
+
+	delete baseDog;
+	delete derivedDog;
+
 	
-
-
-
-
 	return 0;
 
 }
