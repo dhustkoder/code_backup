2b1c0fdbcaf63c443b534ce3a2fd585d45298785
dhustkoder <rafaelmoura.dev@gmail.com>
2015-09-25 13:44:29 +0000

    dynamic cast example 2 (implicit problems) done

--- Cpp/ACPP_List/Casting/Dynamic Casting/example2.cpp
+++ Cpp/ACPP_List/Casting/Dynamic Casting/example2.cpp
@@ -5,17 +5,61 @@ class Dog
 public:
 	virtual ~Dog()
 	{};
-}
+};
+
+class YellowDog : public Dog
+{
+protected:
+	int age;
+public:
+	void bark()
+	{
+		std::cout << "woof." << std::endl; 
+
+	}
+
+
+};
+
+class BlackDog : public YellowDog
+{
+public:
+	void bark()
+	{
+		std::cout << "woof, I am " << age << std::endl;
+	}
+	
+
+};
+
 
 int main()
 {
 
+	Dog* pd = new Dog();
+
+	YellowDog* py = dynamic_cast<YellowDog*>(pd);
+
+	py->bark(); // well, this prints woof.
+	// so it call the function correctly...
+	
+	// BUT
+	std::cout << "pd = " << pd << std::endl; // this prints the objects address 
+	
+	std::cout << "py = " << py << std::endl; // and this prints 0
+
+	// so the dynamic_cast didn't work, and py points to a null pointer. but the function was called...
+	
+	// What happens is that the compiler interprets the "bark" function as a static function cuz we called it from a null pointer and it doesnt access any
+	// data member from Dog or YellowDog.
 
 
+	// But BlackDog access the member age , lets try and see what happens
+	
+	BlackDog *pb = dynamic_cast<BlackDog*>(pd);
 
-
-
-
+	pb->bark(); // what happens is undefined behavior for dereference a null pointer...
+	
 
 
 
