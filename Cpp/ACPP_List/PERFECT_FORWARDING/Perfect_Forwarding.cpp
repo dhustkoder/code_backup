5c2260728e42c89c9b1248d7200dd78d34314320
dhust <rafaelmoura.dev@gmail.com>
2015-10-06 09:39:44 -0300

    Perfect Forwarding Examples

---
+++ Cpp/ACPP_List/PERFECT_FORWARDING/Perfect_Forwarding.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <string>
+
+#define LOG(x) std::cout << x << std::endl;
+
+class MyVec
+{
+public:
+	MyVec(size_t vecSize)
+	{
+		heapPtr_ = new double[vecSize];
+		vecSize_ = vecSize;
+	}
+
+	MyVec(const MyVec &rhs) : // copy constructor
+		MyVec(rhs.vecSize_)
+	{
+		LOG("Called Copy Constructor");
+		std::copy_n(rhs.heapPtr_, rhs.vecSize_, this->heapPtr_);
+	}
+
+	MyVec(MyVec &&rhs) : // Move Constructor
+		heapPtr_(nullptr), vecSize_(0)
+	{
+		LOG("Called Move Constructor");
+		std::swap(this->heapPtr_, rhs.heapPtr_);
+		std::swap(this->vecSize_, rhs.vecSize_);
+	}
+
+
+
+private:
+	double *heapPtr_;
+	size_t vecSize_;
+};
+
+
+
+
+
+MyVec& Process(MyVec &oldVec) // This copies from &oldVec and return the newVec
+{
+	LOG("Lvalue PROCESS");
+	MyVec newVec(oldVec);
+	//............ process
+	return newVec;
+}
+
+MyVec&& Process(MyVec &&oldVec) // This moves oldVec to a newVec and returns the newVec
+{
+	LOG("Rvalue PROCESS");
+	MyVec newVec(std::move(oldVec));
+	//.............process
+	return std::move(newVec);
+}
+
+
+
+
+// So Lets see Perfect Forwarding, in this example we want to pass a object of type MyVec to LogAndProcess Function
+// But in the LogAndProcess the function Process is called, and it takes the same argument we passed to LogAndProcess.
+// So, the point of Perfect Forwarding is, if we passed our object by an Lvalue reference then we want it to be passed to the Process(MyVec&).
+
+// And if we passed by Rvalue, we want it to be passed to the version : Process(MyVec&&)
+
+
+// we can do that like this.
+template<class T>
+T&& LogAndProcess(T&& arg)
+{
+	//......
+	return Process(std::forward<T>(arg));
+	// std::forward is a conditional cast, if arg was passed as Rvalue, it will pass a Rvalue to Process, if is  a Lvalue it will pass a Lvalue to Process
+}
+
+
+int main()
+{
+	MyVec temp(1000);
+	MyVec reusable(2000);
+	LogAndProcess(reusable); // this result in 1 copy operation
+	LogAndProcess(std::move(temp)); // this result in 1 move operation
+
+	// reusable is unchanged
+	// temp is empty
+}
+
+
+
+
+
+
+
