e9d21383d972cec68c2699018dcc4eea6789ab62
dhustkoder <rafaelmoura.dev@gmail.com>
2015-10-24 15:01:42 +0000

    CROSS DELEGATION

---
+++ Cpp/ACPP_List/CROSS_DELEGATION/cross_delegation.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#define LOG(x) std::cout << x << std::endl;
+
+
+
+
+class Storable
+{
+public:
+	Storable(const char* msg) { LOG("Storable Created - " << msg); }
+	virtual void read() = 0; // this becomes pure virtual making storable an abstract
+	virtual void write() =0;// class
+	virtual ~Storable() {LOG("Destroying Storable ...")}
+protected:
+	Storable(){};
+
+private:
+	//....
+
+
+};
+
+
+
+
+class Transmitter : public virtual Storable
+{
+public:
+	Transmitter(const char *msg) : Storable(msg) {LOG("Transmitter Constructor Called ..."); }
+	void write()
+	{
+		LOG("Called Transmitter Write ...");
+		read();
+		// ....
+	}
+	~Transmitter() {LOG("Destroying Transmitter ...")}
+protected:
+	Transmitter() { LOG("Transmitter Default Constructor Called"); }
+
+};
+
+
+
+class Receiver : public virtual Storable
+{
+public:
+	Receiver(const char *msg) : Storable(msg) { LOG("Receiver Constructor Called");}
+	void read()
+	{
+		LOG("Called Receiver Read ...");
+	}
+	~Receiver() { LOG("Destroying Receiver ..."); }
+protected:
+	Receiver() { LOG("Receiver Default Constructor Called "); }
+};
+
+
+
+
+
+
+class Radio : public Transmitter, public Receiver
+{
+public:
+	Radio(const char *msg) : Storable(msg), Transmitter(), Receiver() { LOG("Radio Created ...");}
+	// ...
+	~Radio() {LOG("Destroying Radio ..."); }
+};
+
+
+int main()
+{
+	Radio *radio = new Radio("MSG");
+	
+	Transmitter *transmitter = radio;
+	Receiver *receiver = radio;
+
+
+
+	/*
+		Because of virtual inheritance, when the write() function from the Transmitter Class is Called,
+		the method read() from the Receiver Class gets called (as you may have noticed, 
+		the Transmitter Class doesn't have a read() function).
+
+		In the above Hierarchy we can instantiate only the Receiver and Radio class.
+		Because in the Transmitter class alone, the read() function is pure virtual yet.
+		it only gets concrete function when we instanciate a Receiver, or Radio.
+		cuz Receiver is where the implementation for Read function is.
+
+		but when we Instanciate a Receiver object, Its write function, will be the one in Storable, not in the,
+		Transmitter. So lets make the Write function in Storable pure virtual,
+		and now we cant instanciate a Receiver alone.
+
+
+
+	*/
+
+
+
+
+	
+
+
+
+
+	transmitter->read();
+
+	receiver->write();
+
+
+
+}
