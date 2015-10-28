e8fafaa2fcff8b808010303bccf64edc1c9059c1
dhustkoder <rafaelmoura.dev@gmail.com>
2015-10-28 00:33:10 +0000

    asm update

---
+++ Assembly/simple/Hello World/asm_example.asm
@@ -0,0 +1,1 @@
+section .text	global _start:_start:	mov eax, 3	mov ebx, 1	mov ecx, string	mov edx, length	int 80h	mov eax, 1	mov ebx, 0	int 80hsection .data		string: db 'Hello Asm', 0Ah		length: equ 8
