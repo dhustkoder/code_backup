715131df41fce11daaea778990605a196a03649d
dhustkoder <rafaelmoura.dev@gmail.com>
2015-10-28 16:22:50 +0000

    trying to fix asembly ident

--- Assembly/simple/_9Starts/display_9starts.asm
+++ Assembly/simple/_9Starts/display_9starts.asm
@@ -1,12 +1,12 @@
 
 section .data
-	msg: db 'displaying 9 starts:', 0xa ; a message
-	msg_len: equ $ - msg ; lenght of message
-	s2: times 9 db '*'   ; the starts
-	endl: db 0x0a 		 ; end line value ( to break the line )
+	msg: db 'displaying 9 starts:', 0xa 			; a message
+	msg_len: equ $ - msg 							; lenght of message
+	s2: times 9 db '*' 								; the starts
+	endl: db 0x0a 									; end line value ( to break the line )
 
 section .text
-	global _start ; must be declared for linker (ld) (gcc)
+	global _start 									; must be declared for linker (ld) (gcc)
 
 _start:
 	mov edx, msg_len 	;
@@ -22,7 +22,7 @@ _start:
 	mov ebx, 1 			; stdout
 	int 0x80 			; call kernel
 
-	mov edx, 1 			;
+	mov edx, 1 			; endline is 1 character
 	mov ecx, endl 		; endline value
 	mov eax, 4 			; sys_write
 	mov ebx, 1 			; stdout
