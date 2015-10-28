9370974c3deb20a7ccfbb58fe72add5941ea47e0
dhust <rafaelmoura.dev@gmail.com>
2015-10-28 01:52:52 +0000

    asm up

--- Assembly/simple/Hello World/asm_example.asm
+++ Assembly/simple/Hello World/asm_example.asm
@@ -1,1 +1,35 @@
-section .text	global _start:_start:	mov eax, 3	mov ebx, 1	mov ecx, string	mov edx, length	int 80h	mov eax, 1	mov ebx, 0	int 80hsection .data		string: db 'Hello Asm', 0Ah		length: equ 8
+; An assembly program can be devided into three sections - 
+; 
+; -  The DATA section
+
+; - the BSS section
+
+; - The TEXT section
+
+
+; Text section is used for keeping te actual code. this section must begin with the declaration
+; global _start, which tells the kernel where the program execution begins
+
+section .text
+
+global _start
+
+_start:
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, string
+	mov edx, length
+	int 80h
+	
+	mov eax, 1
+	mov ebx, 0
+	int 80h
+
+
+section .data
+	string: db 'Hello Asm', 0Ah
+	length: equ 8
+
+	; The data section, is used for declaring initialized data or constants. this data
+	; does not change at runtime. you can declare various constants values, file names,
+	; or buffer size, etc.. , in this section is - 
---
+++ Assembly/simple/Hello World/assembly_statements.asm
@@ -0,0 +1,49 @@
+
+
+
+
+; Syntax Of Assembly Statements
+
+; Assembly statements are entered one per line. each one  follows the following format:
+
+; [label] mnemonic [operands] [; comment]
+
+; the fields in the square brackets are optional, A basic instruction has 2 parts
+
+; 1. Name of the instruction ( mnemonic ) 
+
+; 2. operands or parameter of the first command
+
+
+; lets see some typical assembly statements
+
+
+section .bss
+COUNT dw 10
+TOTAL: dw 
+MASK1: dw 
+MARKS: dw 
+
+section .text
+	global _start
+
+_start:
+	inc [COUNT] ; Increment the memory variable COUNT
+
+	mov TOTAL, 48 ; transfer value 48 to memory variable TOTAL
+	
+	add ah, bh ; add the content of BH register to AH register
+
+
+	and MASK1, 128 ; perform AND operation on variable MASK1 and 128
+
+
+	add MARKS, 10 ; add 10 to the variable MARKS
+
+
+
+	mov al, 10 ; transfer value 10 to the al register
+
+	mov eax, 1
+	mov ebx, 0
+	int 0x80
