5696a8eb7b06ccad83fa3b09a4ceaba9b9c78d90
Rafael <rafaelmoura.dev@gmail.com>
2015-12-09 23:40:55 +0000

    C# test with mono

---
+++ Cpp/CsharpMono/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace MonoTest
+{
+    class Node
+    {
+        public int value { get; set; }
+        Node _next = null, _previus = null;
+        public Node next    { get {return _next; } set {_next = value; } }
+        public Node previus { get { return _previus; } set { _previus = value; } }
+        public Node(int x)
+        {
+            value = x;
+        }
+
+        ~Node()
+        {
+            Console.WriteLine("erased node of value " + value);
+        }
+    }
+
+    class List
+    {
+
+
+        private Node head = null;
+        private Node tail = null;
+
+        public List()
+        {
+
+        }
+
+        public void push(int x)
+        {
+            if (head == null)
+            {
+                head = tail = new Node(x);
+                return;
+            }
+
+            var newNode = new Node(x);
+
+            newNode.next = head;
+            head.previus = newNode;
+            head = newNode;
+        }
+
+
+        public void pop()
+        {
+            head = head.next;
+            head.previus = null;
+        }
+
+        public void listNumbers()
+        {
+            for (var itr = head; itr != null;)
+            {
+                Console.WriteLine(itr.value);
+                itr = itr.next;
+            }
+        }
+
+
+
+
+    }
+
+
+
+    class MainClass
+    {
+
+        static void doWork()
+        {
+            List list = new List();
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                list.push(i);
+            }
+
+            list.listNumbers();
+
+        }
+
+        public static void Main(string[] args)
+        {
+            doWork();
+            Console.Read();
+
+            return;
+
+
+        }
+    }
+
+
+}
---
+++ Cpp/CsharpMono/main.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#define LOG(x) std::cout << x << std::endl
+
+class Node
+{
+	friend class List;
+private:
+	Node* next;
+	Node* previus;
+	int value;
+public:
+	Node(int x) : next(nullptr), previus(nullptr), value(x)
+	{
+
+	}
+
+};
+
+class List
+{
+private:
+	Node *head;
+	Node *tail;
+public:
+	List() : head(nullptr), tail(nullptr){}
+
+	template<typename N>
+	void push(N &&x)
+	{
+		if (head == nullptr)
+		{
+			head = tail = new(std::nothrow) Node(std::forward<N>(x));
+			return;
+		}
+
+		auto newNode = new(std::nothrow) Node(std::forward<N>(x));
+
+		newNode->next = head;
+		head->previus = newNode;
+		head = newNode;
+	}
+
+
+	void pop()
+	{
+		auto toDelete = head;
+		head = head->next;
+		head->previus = nullptr;
+		delete toDelete;
+	}
+
+	void listNumbers()
+	{
+		for (auto itr = head; itr != nullptr; itr = itr->next)
+		{
+			printf("%i \n", itr->value);
+		}
+
+	}
+
+	~List()
+	{
+		for (auto itr = head; itr != nullptr; )
+		{
+			auto toDel = itr;
+			itr = itr->next;
+			delete toDel;
+		}
+	}
+
+
+
+};
+
+void doWork()
+{
+	List list;
+
+    for (int i = 0; i < 1000000; i++)
+    {
+        list.push(i);
+    }
+
+	list.listNumbers();
+}
+
+
+int main()
+{
+	doWork();
+	std::cin.ignore();
+
+
+	return 0;
+}
+
