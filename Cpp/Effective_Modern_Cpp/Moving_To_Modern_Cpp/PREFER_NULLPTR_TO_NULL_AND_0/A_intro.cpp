e43312da13bcb62efec50656507956950a69af84
dhust <rafaelmoura.dev@gmail.com>
2015-12-26 10:08:31 -0200

    nullptr implementation explained

--- Cpp/Effective_Modern_Cpp/Moving_To_Modern_Cpp/PREFER_NULLPTR_TO_NULL_AND_0/A_intro.cpp
+++ Cpp/Effective_Modern_Cpp/Moving_To_Modern_Cpp/PREFER_NULLPTR_TO_NULL_AND_0/A_intro.cpp
@@ -35,12 +35,12 @@ void f(int*)
 int main()
 {
 	// so, 0 is a int not a pointer that is why is not what you want
-	// NULL its a macro that can be 0, 0L, and I've seen in MSVC as ((void*)0) which I think its a better than 0
+	// nullptr its a macro that can be 0, 0L, and I've seen in MSVC as ((void*)0) which I think its a better than 0
 
 	
 	f((void*)0); // calls f(void*)
 	f(0);        // calls f(int)
-	//f(NULL);     // in clang and gcc its ambiguos between all f functions
+	//f(nullptr);     // in clang and gcc its ambiguos between all f functions
  
 	//f(nullptr); // calls its ambiguos between  f(void*) and f(int*) -
 	// because nullptr is a pointer of all types will aways be ambiguos between overloads of pointer types
@@ -62,7 +62,7 @@ int main()
 	if(result != 0)   // what is result pointer or integer ?
 		return 0;
 
-	if(result != NULL) // must be a pointer... but can be an integer
+	if(result != nullptr) // must be a pointer... but can be an integer
 		return 0;
 
 
@@ -71,6 +71,9 @@ int main()
 
 
 
+
+
+	
 
 
 }
---
+++ Cpp/Effective_Modern_Cpp/Moving_To_Modern_Cpp/PREFER_NULLPTR_TO_NULL_AND_0/b_nullptr_impl.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#define LOG(x) std::cout << x << std::endl
+	
+
+
+
+
+// nullptr's advantage is that it doesn't have an integral type. To be honest
+// it doesn't have an pointer type, either, but it behaves as a pointer of all types.
+	
+// nullptr actual type is std::nullptr_t and in a wonderfully circular definition,
+// nullptr_t is defined to be the type of nullptr.
+// nullptr_t implicity converts to all pointer types
+
+	
+// the implementation is something like this
+
+
+
+class my_nullptr_t
+{
+public:
+	
+	my_nullptr_t() = default;	
+	
+	template<typename T>
+	constexpr operator T*() const{        // convert to any pointer type
+		return 0;
+	}
+	template<class C, typename T>
+	constexpr operator T C::* () const{  // convert to any pointer to member type
+		return 0;
+	}
+
+private:
+	my_nullptr_t(const my_nullptr_t&) = delete;
+	my_nullptr_t(my_nullptr_t&&) = delete;
+			
+	void operator +() = delete;
+	void operator -() = delete;
+	void operator =(const my_nullptr_t&) = delete;
+	void operator *() = delete;
+	void operator /(const my_nullptr_t&) = delete;
+	void operator &() = delete;
+	void *padding; // ensure the sizeof nullptr is the same as void*
+};
+#define my_nullptr (my_nullptr_t()) 
+// make my_nullptr a rvalue
+
+
+class SomeType
+{
+public:
+	void foo() { LOG("member fun, int x: " << x);}
+	int x;
+};
+
+
+int main()
+{
+	SomeType sometype;
+	
+	int *some_pointer = &sometype.x;                  // common pointer
+	void(SomeType::*PtrToMemFunc)() = &SomeType::foo; // pointer to member
+
+	*some_pointer =  300;       // uses common ptr
+	(sometype.*PtrToMemFunc)(); // uses ptr to member func
+	
+	some_pointer = my_nullptr; // assign nullptr; (uses convert to any pointer type)
+		
+	PtrToMemFunc = my_nullptr; // assign nullptr; (uses convert to any pointer to member type)
+	
+	
+}
