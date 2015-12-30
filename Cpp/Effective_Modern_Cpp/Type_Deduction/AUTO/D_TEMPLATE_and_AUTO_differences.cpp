d64d05e17b59bf84f50d4d5d48456c74e37656a1
dhust <rafaelmoura.dev@gmail.com>
2015-12-30 12:34:35 -0200

    auto and initalizer_list, template and initializer_list

--- Cpp/Effective_Modern_Cpp/Type_Deduction/AUTO/D_TEMPLATE_and_AUTO_differences.cpp
+++ Cpp/Effective_Modern_Cpp/Type_Deduction/AUTO/D_TEMPLATE_and_AUTO_differences.cpp
@@ -15,6 +15,15 @@ constexpr void printTypes() noexcept
 
 }
 
+template<typename T>
+void foo(T param) {
+
+}
+template<typename T>
+void foo2(std::initializer_list<T> param) {
+
+}
+
 int main()
 {
 	// so auto and templates are almost the same except for some situations 
@@ -45,7 +54,38 @@ int main()
 	printTypes<DCT(x1), DCT(x2), DCT(x3), DCT(x4)>();
 
 	// these declarations all compile, but they don't have the same meaning as the ones they replace.
-	// the first    	
+	// the first 2 statements will, indeed, declare a variable of type int, with value 27
+	// the second 2 statements will declare a initializer_list<int>
+	// in C++17 the x4 would be a int
+
+	// C++17
+	auto var1 = { 27 }; // initializer_list<int>
+	auto var2{ 27 };    // int
+
+	// This happens because of a special type deduction rule for auto.
+	// : When the initializer for a auto-declared variable is enclosed in braces,
+	// the deduced type is initializer_list. If such type can't be deduced the code
+	// will be rejected:
+
+	auto var3 = { 10, 20, 3.0 }; // error
+	// that happens because is 2 type deduction happening
+	// one for auto that happens to be initializer_list<T>
+
+	// and then the type of T in initializer_list<T> that must be 1 type
+
+	// that's the way template and auto differ, because if you try yo call
+	// a template function with braced initializer trying to get
+	// the template deduce the type for T as initializer_list the code will fail
+
+
+	foo({ 10,20,30 }); // can't deduce type for T...
+	auto var4 = { 10,20,30 }; // var4 is initializer_list<int>
+
+	// but if you try with ParamType as std::initializer_list<T>
+	// template<typename T> void foo2(std::initializer_list<T> param) {}
+
+	// then it works
+	foo2({ 10,20,30 }); // works, T is int, ParamType is std::initializer_list<int>
 
 
 }
