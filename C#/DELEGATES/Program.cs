a7c394f0f764dcbbfa6d31daf043ec03613d4874
dhust <rafaelmoura.dev@gmail.com>
2016-02-08 02:14:43 -0200

    Custom C# Delegate class in C++

---
+++ C#/DELEGATES/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DELEGATES
+{
+    // delegates are special classes
+    // that serve as pointer to methods
+    // example
+    public delegate int CalcDelegate(int a, int b);
+
+    public static class Test
+    {
+        public static void Process(CalcDelegate c)
+        {
+            Console.WriteLine(c(10, 10));
+        }
+    }
+
+    class CallDelegates
+    {
+        public void Execute()
+        {
+            var sum = new CalcDelegate(Sum);
+            var sub = new CalcDelegate(Sub);
+            var mul = new CalcDelegate(Mul);
+
+            Test.Process(sum);
+            Test.Process(sub);
+            Test.Process(mul);
+
+            // or
+            Console.WriteLine(sum(6, 6) + sub(4, 10) + mul(10, 10));
+
+            // we can also add more func to 1 delegate
+            var allCalcs = sum + sub + mul;
+
+            Console.WriteLine(allCalcs(10, 10));
+        }
+
+
+        int x = 10;
+
+        int Sum(int a, int b)
+        {
+            return x += (a + b);
+        }
+        int Sub(int a, int b)
+        {
+            return x += (a - b);
+        }
+        int Mul(int a, int b)
+        {
+            return x += (a * b);
+        }
+
+    }
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            CallDelegates x = new CallDelegates();
+            x.Execute();
+        }
+    }
+}
---
+++ Cpp/Effective_Modern_Cpp/MY_DELEGATE_EXAMPLE_CPP/delegate.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+#include <vector>
+
+template<class T, class ReturnType, class ...Args>
+using MethodPtr = ReturnType (T::*)(Args...);
+
+
+template<class T, class ReturnType, class ...Ts>
+class DelegateBase
+{
+protected:
+	T& m_ref;
+	using TMethod = MethodPtr<T, ReturnType, Ts...>;
+	std::vector<TMethod> m_methods;
+public:
+	DelegateBase(T& ref) :
+		m_ref(ref)
+	{
+
+	}
+
+	void operator+=(TMethod method)
+	{
+		m_methods.push_back(method);
+	}
+
+	void operator=(TMethod method)
+	{
+		m_methods.clear();
+		m_methods.push_back(method);
+	}
+};
+
+
+template<class T, class ReturnType, class ...Ts>
+class Delegate : public std::enable_if_t<!std::is_void<ReturnType>::value, DelegateBase<T, ReturnType, Ts...>>
+{
+	using Base = DelegateBase<T, ReturnType, Ts...>;
+public:
+	Delegate(T& ref) : DelegateBase<T, ReturnType, Ts...>(ref) {}
+
+	ReturnType operator()(Ts&&...args)
+	{
+		ReturnType result = ((Base::m_ref).*(Base::m_methods[0]))(std::forward<Ts>(args)...);
+
+		for (auto itr = Base::m_methods.begin() + 1; itr != Base::m_methods.end(); ++itr)
+		{
+			result += ((Base::m_ref).*((*itr)))(std::forward<Ts>(args)...);
+		}
+
+		return result;
+	}
+
+	using Base::operator=;
+
+};
+
+
+// void specialization
+template<class T, class ...Ts>
+class Delegate<T, void, Ts...> : public DelegateBase<T, void, Ts...>
+{
+	using Base = DelegateBase<T, void, Ts...>;
+public:
+	Delegate(T& ref) : DelegateBase<T, void, Ts...>(ref) {}
+	void operator()(Ts&&...args)
+	{
+
+		for (auto& method : Base::m_methods)
+		{
+			((Base::m_ref).*(method))(std::forward<Ts>(args)...);
+		}
+	}
+	using Base::operator=;
+
+};
+
+
+
+
+// -----------------------------------------------------------
+// Example:
+class OtherClass
+{
+public:
+	template<class T, class ...Ts>
+	auto DoSomething(T x, Ts&& ...args)
+	{
+		return x(std::forward<Ts>(args)...);
+	}
+};
+
+
+class Test
+{
+	int x = 10;
+
+	int Sum(int a, int b)
+	{
+		return x += (a + b);
+	}
+
+	int Sub(int a, int b)
+	{
+		return x += (a - b);
+	}
+
+	int Mul(int a, int b)
+	{
+		return x += (a * b);
+	}
+
+	void InternalMethod()
+	{
+		std::cout << "Hi, I'm Test's private method" << std::endl;
+	}
+
+public:
+	auto callDelegate()
+	{
+		OtherClass otherObject;
+		auto *del = new Delegate<Test,int, int, int>(*this);
+		*del = &Test::Sum;
+		*del += &Test::Sub;
+		*del += &Test::Mul;
+		
+		// other object calls Test's private method through Delegate
+		std::cout << otherObject.DoSomething(*del, 10,10) << std::endl;
+
+
+		// void return type method...
+		auto *del2 = new Delegate<Test, void>(*this);
+		*del2 = &Test::InternalMethod;
+		
+		otherObject.DoSomething(*del2);
+	}
+
+};
+
+
+
+
+int main()
+{
+	Test().callDelegate();
+}
